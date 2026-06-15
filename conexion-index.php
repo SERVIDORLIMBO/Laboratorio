e34b4f08ee039d903351a02bb3f8971ead187f82
SERVIDORLIMBO <vortexcuentamods@gmail.com>
2026-06-14 19:45:41 -0600

    Add database connection script in conexion-index.php

---
+++ conexion-index.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$db = "laboratorio_quimica";
+$user = "root";
+$pass = "";
+try {
+    $pdo = new PDO (
+        dsn: "mysql:host=$host;port=3306;dbname=$db;charset=utf8",
+        username: $user,
+        password: $pass);
+    $pdo->setAttribute(attribute: PDO::ATTR_ERRMODE,
+                    value: PDO::ERRMODE_EXCEPTION);
+}   catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
